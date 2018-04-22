919606ee919ef9d1a2a5669ae601f622bc6942cb
smaillet <smaillet@users.noreply.github.com>
2018-04-22 15:24:26 -0700

    fixed path to initialize-buildenv.ps1 in Build-CI.ps1

--- scripts/Build-CI.ps1
+++ scripts/Build-CI.ps1
@@ -5,7 +5,7 @@ Write-Information 'Updating sub-modules for repository...'
 git submodule -q update --init --recursive
 
 Write-Information 'Initializing Build Environment...'
-.\Initialize-BuildEnv.ps1
+.\scripts\Initialize-BuildEnv.ps1
 
 Write-Information -Message 'Building...'
 Invoke-Build -Publish Project
