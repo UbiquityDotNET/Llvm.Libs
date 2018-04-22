294e866473904c51f122cc4ec3ebb670a806936a
smaillet <smaillet@users.noreply.github.com>
2018-04-22 16:43:39 -0700

    Added nuget source with build username to enable access to account feed. (It's all public stuff - nothing private so no big deal)

--- scripts/Build-CI.ps1
+++ scripts/Build-CI.ps1
@@ -7,5 +7,7 @@ git submodule -q update --init --recursive
 Write-Information 'Initializing Build Environment...'
 .\scripts\Initialize-BuildEnv.ps1
 
+invoke-nuget sources Add -Source https://ci.appveyor.com/nuget/UbiquityDotNet/api/v2/package -Username $env:acctfeed_username -Password $env:acct_feed_pw
+
 Write-Information -Message 'Building...'
 Invoke-Build -Publish Project
