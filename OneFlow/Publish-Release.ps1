65724585a59133c75cb9e6b4fc4dfdef50755bf5
Steven Maillet <smaillet@users.noreply.github.com>
2025-07-14 14:35:27 -0700

    Updated OneFlow scripts based on experience in other repositories (#45)

--- OneFlow/Publish-Release.ps1
+++ OneFlow/Publish-Release.ps1
@@ -19,9 +19,12 @@ using module '../PsModules/RepoBuild/RepoBuild.psd1'
 .NOTE
     For the gory details of this process see: https://www.endoflineblog.com/implementing-oneflow-on-github-bitbucket-and-gitlab
 #>
-
+[cmdletbinding(SupportsShouldProcess=$True, ConfirmImpact='High')]
 Param()
 $buildInfo = Initialize-BuildEnvironment
+
+Write-Information "Build Version XML:"
+Write-Information ((Get-ParsedBuildVersionXML -BuildInfo $buildInfo).GetEnumerator() | Sort -Property Name | Out-String)
 
 # merging the tag to develop branch on the official repository triggers the official build and release of the NuGet Packages
 $tagName = Get-BuildVersionTag $buildInfo
@@ -39,30 +42,45 @@ $mergeBackBranchName = "merge-back-$tagName"
 Write-Information 'Fetching from official repository'
 Invoke-External git fetch $officialRemoteName
 
-Write-Information "Switching to release branch [$officialReleaseBranch]"
-Invoke-External git switch '-C' $releasebranch $officialReleaseBranch
-
-$confirmation = Read-Host "Are you Sure You Want To Proceed:"
-if ($confirmation -ne 'y')
+if($PSCmdlet.ShouldProcess($officialReleaseBranch, "git switch -C $releaseBranch"))
 {
-    Write-Host "User canceled operation"
-    return
+    Write-Information "Switching to release branch [$officialReleaseBranch]"
+    Invoke-External git switch '-C' $releaseBranch $officialReleaseBranch
 }
 
-Write-Information 'Creating tag of this branch as the release'
-Invoke-External git tag $tagName '-m' "Official release: $tagName"
+if($PSCmdlet.ShouldProcess($tagName, "create signed tag"))
+{
+    Write-Information 'Creating a signed tag of this branch as the release'
+    Invoke-External git tag -s $tagName '-m' "Official release: $tagName"
 
-Write-Information 'Pushing tag to official remote [Starts automated build release process]'
-Invoke-External git push $officialRemoteName '--tags'
+    Write-Information 'Verifying signature on tag'
+    Invoke-External git tag -v $tagName
+}
 
-Write-Information 'Creating local merge-back branch to merge changes associated with the release'
-# create a "merge-back" child branch to handle any updates/conflict resolutions when applying
-# the changes made in the release branch back to the develop branch.
-Invoke-External git checkout '-b' $mergeBackBranchName $releasebranch
-Write-Information 'pushing merge-back branch to fork'
-Invoke-External git push $forkRemoteName $mergeBackBranchName
+if($PSCmdlet.ShouldProcess($tagName, "git push $officialRemoteName tag"))
+{
+    Write-Information 'Pushing tag to official remote [Starts automated build release process]'
+    Invoke-External git push $officialRemoteName tag $tagName
+}
 
-Write-Information 'Fast-forwarding main to tagged release'
-Invoke-External git switch '-C' $mainBranchName $officialMainBranch
-Invoke-External git merge --ff-only $tagName
-Invoke-External git push $officialRemoteName $mainBranchName
+if($PSCmdlet.ShouldProcess($releasebranch, "git checkout -b $mergeBackBranchName"))
+{
+    Write-Information 'Creating local merge-back branch to merge changes associated with the release'
+    # create a "merge-back" child branch to handle any updates/conflict resolutions when applying
+    # the changes made in the release branch back to the develop branch.
+    Invoke-External git checkout '-b' $mergeBackBranchName $releasebranch
+}
+
+if($PSCmdlet.ShouldProcess("$forkRemoteName $mergeBackBranchName", "git push"))
+{
+    Write-Information 'pushing merge-back branch to fork'
+    Invoke-External git push $forkRemoteName $mergeBackBranchName
+}
+
+if($PSCmdlet.ShouldProcess("$tagName", "Reset main to point to release tag"))
+{
+    Write-Information 'Updating main to point to tagged release'
+    Invoke-External git switch '-C' $mainBranchName $officialMainBranch
+    Invoke-External git reset --hard $tagName
+    Invoke-External git push --force $officialRemoteName $mainBranchName
+}
