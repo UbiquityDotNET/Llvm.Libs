e7053b2e0e20d463632b67def8da319a7aa2d0ee
smaillet <smaillet@users.noreply.github.com>
2025-07-14 14:34:04 -0700

    Updated OneFlow scripts based on experience in other repositories

--- OneFlow/Start-Release.ps1
+++ OneFlow/Start-Release.ps1
@@ -12,8 +12,10 @@ using module '../PsModules/RepoBuild/RepoBuild.psd1'
 .DESCRIPTION
     This function creates and publishes a Release branch
 #>
-
-Param([string]$commit = "")
+[cmdletbinding(SupportsShouldProcess=$True)]
+Param(
+    [string]$commit = ""
+)
 $buildInfo = Initialize-BuildEnvironment
 
 $officialRemoteName = Get-GitRemoteName $buildInfo official
@@ -24,18 +26,24 @@ $branchName = "release/$(Get-BuildVersionTag $buildInfo)"
 Write-Information "Creating release branch in local repo"
 if ([string]::IsNullOrWhiteSpace($commit))
 {
-    Invoke-External git checkout -b $branchName
+    if($PSCmdlet.ShouldProcess($branchName, "git checkout -b"))
+    {
+        Invoke-External git checkout -b $branchName
+    }
 }
 else
 {
-    Invoke-External git checkout -b $branchName $commit
+    if($PSCmdlet.ShouldProcess("$branchName $commit", "git checkout -b"))
+    {
+        Invoke-External git checkout -b $branchName $commit
+    }
 }
-
-# push to fork so that changes go through normal PR process
-Write-Information "Pushing branch to fork"
-Invoke-External git push $forkRemoteName -u $branchName
 
 # Push to product repo so it is clear to all a release
 # is in process.
 Write-Information "Pushing branch to official repository (Push/Write permission required)"
-Invoke-External git push $officialRemoteName $branchName
+if($PSCmdlet.ShouldProcess("$officialRemoteName $branchName", "git push"))
+{
+    Invoke-External git push $officialRemoteName $branchName
+}
+
